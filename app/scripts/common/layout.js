87af22b26b1c29ffbabdd0468ca0ebb8e83b1d41
scarletsky <scarletsky1025@gmail.com>
2014-09-23 10:24:12 +0800

    feat: add sticky header

--- app/scripts/common/layout.js
+++ app/scripts/common/layout.js
@@ -1,8 +1,35 @@
 angular.module('bdLayout', [])
+
+  .directive('bdFixedHeader', [
+    bdFixedHeaderDirective
+  ])
+
   .directive('bdFixedFooter', [
     '$window',
     bdFixedFooterDirective
   ]);
+
+function bdFixedHeaderDirective () {
+  return {
+    scope: false,
+    link: function ($scope, $element, $attrs) {
+
+      var containerElement = $('material-content');
+
+      containerElement.on('scroll', function (e) {
+        var scrollTop = e.target.scrollTop;
+
+        if (scrollTop > 112) {
+          $element.addClass('header-fixed');
+          containerElement.addClass('scrolling');
+        } else {
+          $element.removeClass('header-fixed');
+          containerElement.removeClass('scrolling');
+        }
+      });
+    }
+  };
+}
 
 function bdFixedFooterDirective ($window) {
 
